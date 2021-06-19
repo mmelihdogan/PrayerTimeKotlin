03dc3f0b373fa76cc6d62377ae338e755f2f80bb
Melih Dogan <melih.dogan@boun.edu.tr>
2021-06-19 21:47:34 +0300

    Created prayer time data model

---
+++ app/src/main/java/com/melihd/prayertimekotlin/model/PrayerTimeModel.kt
@@ -0,0 +1,12 @@
+package com.melihd.prayertimekotlin.model
+
+data class PrayerTimeModel(val id : Int,
+                           val city : String,
+                           val date : String,
+                           val fajr : String,
+                           val sun : String,
+                           val dhuhr : String,
+                           val asr : String,
+                           val maghrib : String,
+                           val isha : String)
+
